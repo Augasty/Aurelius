a125641424788b23acbf8363dc47c56296fad4ea
Augasty <sayaksengupta140@gmail.com>
2023-12-23 16:36:21 +0530

    not working, try after moving to ts

---
+++ src/testing/App.jsx
@@ -0,0 +1,21 @@
+// src/App.jsx
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, Vite + React!</h1>
+      <p>Edit src/App.jsx and save to reload.</p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </div>
+  );
+};
+
+export default App;
