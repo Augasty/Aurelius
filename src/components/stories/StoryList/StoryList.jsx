8920e7546c08930796da28a000f04ab1f12d1fb3
Augasty <sayaksengupta140@gmail.com>
2024-01-05 00:22:17 +0530

    all functionalities added, now we just need to design the ui

--- src/components/stories/StoryList/StoryList.jsx
+++ src/components/stories/StoryList/StoryList.jsx
@@ -6,29 +6,36 @@ import { useSelector } from "react-redux";
 import { useProjectContexts } from "../../../utils/ProjectContexts";
 import { useEffect, useState } from "react";
 import TaskContainerStory from "../TaskContainerStory/TaskContainerStory";
+import { separateStories } from "./formatStoryArray";
 
 const StoryList = () => {
   const { isRightPanelVisible } = useProjectContexts();
   const [storyDisplayedTime, setstoryDisplayedTime] = useState("createdAt");
+  const [filterName, setfilterName] = useState(null);
 
   const reduxstories = useSelector((state) => state.stories);
-  var filteredStories = reduxstories.filter((obj) => !obj.dummy);
-  useEffect(() => {
-    filteredStories.sort(
-      (a, b) =>
-        new Date(b[storyDisplayedTime]) - new Date(a[storyDisplayedTime])
-    );
+  let [filteredStories, setfilteredStories] = useState([]);
 
-    console.log(filteredStories);
+  useEffect(() => {
+    console.log(storyDisplayedTime, filterName);
+
+    const filter = separateStories(
+      reduxstories,
+      storyDisplayedTime,
+      filterName
+    );
+    setfilteredStories(filter);
+
+    console.log(filter);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [storyDisplayedTime]);
+  }, [storyDisplayedTime, filterName]);
 
   const [createdAtShown, setcreatedAtShown] = useState(false);
 
   return (
     <div style={{ display: "grid" }}>
-      {/* <div className={filterbarStyles.FilterBar}>
-        <p className={filterbarStyles.FilterHeaderText}>Show Task Date By::</p>
+      <div className={filterbarStyles.FilterBar}>
+        <p className={filterbarStyles.FilterHeaderText}>Task Date:</p>
         <button
           className={filterbarStyles.FilterButton}
           onClick={() => setcreatedAtShown(!createdAtShown)}
@@ -36,59 +43,64 @@ const StoryList = () => {
           {createdAtShown ? "Created At" : "Updated At"}
         </button>
 
-        <p className={filterbarStyles.FilterHeaderText}>Show Story Date By:</p>
-
-        <>
-          <button
-            className={`${filterbarStyles.FilterButton} ${
-              storyDisplayedTime === "createdAt" &&
-              filterbarStyles.ActiveFilterButton
-            }`}
-            onClick={() => setstoryDisplayedTime("createdAt")}
-          >
-            Created At
-          </button>
-
-          <button
-            className={`${filterbarStyles.FilterButton} ${
-              storyDisplayedTime === "updatedAt" &&
-              filterbarStyles.ActiveFilterButton
-            }`}
-            onClick={() => setstoryDisplayedTime("updatedAt")}
-          >
-            Updated At
-          </button>
-
-          <button
-            className={`${filterbarStyles.FilterButton} ${
-              storyDisplayedTime === "deadline" &&
-              filterbarStyles.ActiveFilterButton
-            }`}
-            onClick={() => setstoryDisplayedTime("deadline")}
-          >
-            Deadline
-          </button>
-        </>
-
-        <p className={filterbarStyles.FilterHeaderText}></p>
-
-        <button className={filterbarStyles.FilterButton} onClick={() => {}}>
-          Filter Finished Stories
+        <p className={filterbarStyles.FilterHeaderText}>Story Date:</p>
+        <button
+          className={filterbarStyles.FilterButton }
+          onClick={() => {
+            if (storyDisplayedTime === "createdAt") {
+              setstoryDisplayedTime("updatedAt");
+            } else if (storyDisplayedTime === "updatedAt") {
+              setstoryDisplayedTime("deadline");
+            } else {
+              setstoryDisplayedTime("createdAt");
+            }
+          }}
+        >
+          {storyDisplayedTime}
         </button>
-      </div> */}
+
+        <p className={filterbarStyles.FilterHeaderText}>Filter:</p>
+
+        <button
+          className={`${filterbarStyles.FilterButton} ${
+            filterName === "completionCount" &&
+            filterbarStyles.ActiveFilterButton
+          }`}
+          onClick={() =>
+            setfilterName(
+              filterName === "completionCount" ? null : "completionCount"
+            )
+          }
+        >
+          Active
+        </button>
+
+        <button
+          className={`${filterbarStyles.FilterButton} ${
+            filterName === "deadline" && filterbarStyles.ActiveFilterButton
+          }`}
+          onClick={() =>
+            setfilterName(filterName === "deadline" ? null : "deadline")
+          }
+        >
+          Overdue
+        </button>
+      </div>
 
       <div
         className={styles.storyList}
         style={{ width: isRightPanelVisible ? "74vw" : "98vw" }}
       >
-        {filteredStories.map((story, index) => (
-          <TaskContainerStory
-            key={index}
-            story={story}
-            storyDisplayedTime={storyDisplayedTime}
-            createdAtShown={createdAtShown}
-          />
-        ))}
+        {(filteredStories.length > 0 ? filteredStories : reduxstories).map(
+          (story, index) => (
+            <TaskContainerStory
+              key={index}
+              story={story}
+              storyDisplayedTime={storyDisplayedTime}
+              createdAtShown={createdAtShown}
+            />
+          )
+        )}
       </div>
     </div>
   );
