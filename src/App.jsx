696d3e68f2d40f18d8e6703d775d9394f5d8b43e
Augasty <sayaksengupta140@gmail.com>
2024-01-21 21:25:24 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,6 @@ import './App.css';
 
 function App() {
 
-  // useEffect(() => {
-  //   console.log(isDarkMode);
-  //   const body = document.body;
-  //   if (isDarkMode) {
-  //     body.classList.add('dark-mode');
-  //   } else {
-  //     body.classList.remove('dark-mode');
-  //   }
-  // }, [isDarkMode]);
-
 
   return <Routing />;
 }
