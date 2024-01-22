5164903e048f548d38fe4a3912f5f2b5772326d9
Augasty <sayaksengupta140@gmail.com>
2024-01-22 20:39:31 +0530

    restructured

---
+++ src/components/navbar/Theme/Theme.jsx
@@ -0,0 +1,19 @@
+
+import { useProjectContexts } from '../../../utils/ProjectContexts';
+import styles from './Theme.module.css';
+
+const ToggleSwitch = () => {
+  const {isDarkMode, setIsDarkMode} = useProjectContexts();
+  const handleToggle = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  return (
+    <label className={`${styles.toggleSwitch} ${isDarkMode ? styles.active : ''}`}>
+      <input type="checkbox" onChange={handleToggle} />
+      <span className={styles.slider}></span>
+    </label>
+  );
+};
+
+export default ToggleSwitch;
